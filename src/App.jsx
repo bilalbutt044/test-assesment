f7550a42b4654e55416d13f73270c7a165eaba7d
Muhammad Bilal <bilal.soft000@gmail.com>
2022-03-31 18:06:12 +0500

    infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,64 @@
-import { useState, useEffect } from "react"
-import './App.css';
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
 
 function App() {
-  const [photos, setPhotos] = useState([])
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1)
+
 
   useEffect(() => {
-    getPhotos()
-  }, [])
+    getPhotos();
+  }, []);
 
   const getPhotos = () => {
-    fetch('https://api.unsplash.com/photos?w=300&h=300', {
+
+    fetch(`https://api.unsplash.com/photos?w=300&h=300&page=${page}`, {
       headers: {
-        'Authorization': `Client-ID ${process.env.REACT_APP_CLIENT_ID}`
-      }
+        Authorization: `Client-ID ${process.env.REACT_APP_CLIENT_ID}`,
+      },
+      
     })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setPhotos(data)
-      })
-  }
+      .then((res) => res.json())
+      .then((data) => {
 
 
+        setPhotos((prev) =>  [...prev, ...data]);
+        setPage(prev => prev + 1)
+      });
+  };
 
   return (
     <div className="App">
-
       <div className="search-filter">
         <div className="search">
-          <input type='text'  />
+          <input type="text" />
           <button>Search</button>
         </div>
 
         <div className="filter">
-            <select>
-              <option>Page size</option>
-              <option>category</option>
-              <option>tags</option>
-            </select>
+          <select>
+            <option>Page size</option>
+            <option>category</option>
+            <option>tags</option>
+          </select>
         </div>
       </div>
 
-      <div className="grid">
-        {photos.map(p => (
-        <div className="image">
-          <img src={p?.urls?.thumb} alt="random pic" loading="lazy" />
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={getPhotos}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="grid">
+          {photos.map((p) => (
+            <div className="image">
+              <img src={p?.urls?.thumb} alt="random pic" loading="lazy" />
+            </div>
+          ))}
         </div>
-        ))}
-
-      </div>
+      </InfiniteScroll>
     </div>
   );
 }
